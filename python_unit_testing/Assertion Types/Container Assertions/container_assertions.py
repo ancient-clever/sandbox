4720db199c26adfaf63e64372ec06899782f71cf
ancient-clever <ancient-clever@outlook.com>
2019-10-16 20:30:26 +0300

    container lesson

--- python_unit_testing/Assertion Types/Container Assertions/container_assertions.py
+++ python_unit_testing/Assertion Types/Container Assertions/container_assertions.py
@@ -69,24 +69,30 @@ class TestRandomShuffle(unittest.TestCase):
     def test_empty(self):
         """check that giving an empty list results in an empty list"""
         print("empty")
-        implement this test!
+        self.assertCountEqual(random_shuffle([]), [])
 
     def test_same_length(self):
         """check that the returned list is of the same length as the one givne"""
         print("same")
-        implement this test!
+        values = [1, 1, 1, 1]
+        self.assertEqual(len(random_shuffle(values)), 4)
 
     def test_same_values(self):
         """check that the values in the return list are the same"""
         print("same value")
-        implement this test!
+        values = [1, 1, 1, 1]
+        self.assertCountEqual(random_shuffle(values), values)
 
     def test_values_are_shuffled(self):
         """check that the function does actually return the values in a different order"""
         print("shuffled")
-        implement this test!
+        values = list(range(100))
+        values_copy = values.copy()
+        self.assertNotEqual(random_shuffle(values), values_copy)
 
     def test_input_not_mutated(self):
         """check that the input list of values is not mutated"""
         print("mutated")
-        implement this test!
+        values = list(range(100))
+        random_shuffle(values)
+        self.assertEqual(values, list(range(100)))
