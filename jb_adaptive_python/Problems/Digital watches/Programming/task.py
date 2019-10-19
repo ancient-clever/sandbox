836dc8844913073eaed29dfd95f6df3f5d15c076
ancient-clever <ancient-clever@outlook.com>
2019-10-19 18:57:55 +0300

    digital watches

--- jb_adaptive_python/Problems/Digital watches/Programming/task.py
+++ jb_adaptive_python/Problems/Digital watches/Programming/task.py
@@ -1,1 +1,26 @@
 # put your python code here
+
+
+def watches(seconds):
+    """(int) -> str
+
+    Return time in h:mm:ss format from given seconds
+
+    watches(3602):
+    >>> '1:00:02'
+    """
+    h = (seconds//3600) % 24
+    m = (seconds//60) % 60
+    if m < 10:
+        m = '0{}'.format(m)
+    s = seconds % 60
+    if s < 10:
+        s = '0{}'.format(s)
+
+    return '{}:{}:{}'.format(h, m, s)
+
+
+if __name__ == '__main__':
+    import sys
+    s = int(sys.stdin.readline())
+    print(watches(s))
