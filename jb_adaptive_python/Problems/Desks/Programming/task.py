7e0e594b6a0248304db28692727ba39dbaca7ffd
ancient-clever <ancient-clever@outlook.com>
2019-10-20 15:00:37 +0300

    desks task

--- jb_adaptive_python/Problems/Desks/Programming/task.py
+++ jb_adaptive_python/Problems/Desks/Programming/task.py
@@ -1,1 +1,23 @@
 # put your python code here
+
+
+def count_desks(students):
+    """(list of int) -> int
+
+    Return number of desks for classes for given students
+
+    >>> count_desks([20, 21, 22])
+    32
+    >>> count_desks([16, 18, 20])
+    27
+    """
+    result = 0
+    for s in students:
+        result += (s//2 + s % 2)
+    return round(result)
+
+
+if __name__ == '__main__':
+    import sys
+    n = [int(i) for i in sys.stdin]
+    print(count_desks(n))
