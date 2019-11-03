103961856b258d623a929f582a520a0b6d654b7e
ancient-clever <ancient-clever@outlook.com>
2019-11-03 12:41:08 +0300

    django project update

--- web_technology/ask/ask/urls.py
+++ web_technology/ask/ask/urls.py
@@ -14,8 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
+from django.contrib import admin
+admin.autodiscover()
+
+import ask.views
+import ask.urls_constants
+import qa.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    # /
+    path(r'^$', qa.views.new, name=ask.urls_constants.NEW_URL_NAME),
+    # /login/
+    path(r'^login/', ask.views.stub_ok, name=ask.urls_constants.LOGIN_URL_NAME),
+    # /signup/
+    path(r'^signup/', ask.views.stub_ok, name=ask.urls_constants.SIGNUP_URL_NAME),
+    # /question/<123>/    # instead of <123> it must be a question_id
+    path(r'^question/(?P<question_id>[0-9]+)/', qa.views.question_details, name=ask.urls_constants.QUESTION_DETAILS_URL_NAME),
+    # /ask/
+    path(r'^ask/', ask.views.stub_ok, name=ask.urls_constants.ASK_URL_NAME),
+    # /popular/
+    path(r'^popular/', qa.views.popular, name=ask.urls_constants.POPULAR_URL_NAME),
+    # /admin/
+    path(r'^admin/', include(admin.site.urls)),
 ]
