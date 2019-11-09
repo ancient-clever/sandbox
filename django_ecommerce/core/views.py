47aaf97547e7a85df88b6cd7b6d558380f641912
ancient-clever <ancient-clever@outlook.com>
2019-11-10 02:04:07 +0300

    product listview and detailview

--- django_ecommerce/core/views.py
+++ django_ecommerce/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
 
@@ -6,12 +7,18 @@ def products(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, "products.html", context)
+    return render(request, "product.html", context)
 
 
 def checkout(request):
     return render(request, "checkout.html")
 
 
-def home(request):
-    return render(request, "home.html")
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
