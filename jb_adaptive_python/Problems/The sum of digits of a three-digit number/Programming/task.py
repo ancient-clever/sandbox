0a620aafecf059fa48f7ab518980d3ece34f43a3
ancient-clever <ancient-clever@outlook.com>
2020-01-21 20:50:02 +0300

    new task complete

--- jb_adaptive_python/Problems/The sum of digits of a three-digit number/Programming/task.py
+++ jb_adaptive_python/Problems/The sum of digits of a three-digit number/Programming/task.py
@@ -1,1 +1,20 @@
 # put your python code here
+
+
+def sum_of_three(n, result=0):
+    """(int) -> int
+
+    Return the sum of three given digits.
+
+    >>> sum_of_three(476)
+    17
+    """
+    for digit in n:
+        result += int(digit)
+    print(result)
+
+
+if __name__ == "__main__":
+    import sys
+    n = sys.stdin.readline()
+    sum_of_three(n)
