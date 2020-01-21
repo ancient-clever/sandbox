6da49e44c16ee01dad22840265e1277788c08bb6
ancient-clever <ancient-clever@outlook.com>
2020-01-21 20:50:12 +0300

    new task complete

--- jb_adaptive_python/Problems/Next even number/Programming/task.py
+++ jb_adaptive_python/Problems/Next even number/Programming/task.py
@@ -1,1 +1,24 @@
 # put your python code here
+
+
+def natural_n(n):
+    """(int) -> int
+
+    Return even number following given N.
+
+    >>> natural_n(7)
+    8
+    >>> natural_n(8)
+    10
+    """
+    number = n + 1
+    if not number % 2:
+        return number
+    return number + 1
+
+
+if __name__ == "__main__":
+    import sys
+    n = int(sys.stdin.readline())
+    result = natural_n(n)
+    print(result)
